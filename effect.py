b7cba5f64e35141deed21d1e43ee7e4c6280eb0f
Jean-Baptiste Clement <jean-baptiste.clement.c@thalesdigital.io>
2025-06-25 16:58:39 +0200

    new version using numba

---
+++ effect.py
@@ -0,0 +1,46 @@
+import pygame
+import sys
+from fire import Fire
+
+width = 800
+height = 600
+screen_width = 800
+screen_height = 600
+
+
+class App:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((screen_width, screen_height),0,8)
+        self.clock = pygame.time.Clock()
+        self.fire = Fire(self)
+
+    def update(self):
+        self.fire.update()
+        self.clock.tick()
+        #pygame.display.set_caption(f'{self.clock.get_fps() : .1f}')
+
+    def draw(self):
+        self.fire.draw()
+        pygame.display.flip()
+
+    def get_time(self):
+        self.time = pygame.time.get_ticks() * 0.001
+
+    def check_event(self):
+        for i in pygame.event.get():
+            if i.type == pygame.QUIT or (i.type == pygame.KEYDOWN and i.key == pygame.K_ESCAPE):
+                pygame.quit()
+                sys.exit()
+
+    def run(self):
+        while True:
+            self.check_event()
+            self.get_time()
+            self.update()
+            self.draw()
+
+
+if __name__ == '__main__':
+    app = App()
+    app.run()
--- fire.py
+++ fire.py
@@ -1,82 +1,76 @@
 import pygame
 import numpy as np
 import random
-
-width = 80
-height = 50
-screen_width = 800
-screen_height = 500
-membuffer = np.zeros((width,height), np.int8)
-colour_map = np.zeros((256, 3))
-
-def add_fire():
-     
-    for x in range(0,width):
-       buffer[x][height-1] = random.randint(0,255)          
-
-def fire():
-# Créer un tableau pour stocker les valeurs calculées
-    result = np.zeros_like(buffer)
-
-# Parcourir les indices x et y, mais exclure les bords
-    for x in range(0, width):
-        for y in range(0, height):
-            result[x, y] = (buffer[(x-1)%width, (y+1)%height] + buffer[x, (y+1)%height] + buffer[(x+1)%width, (y+1)%height] + buffer[x, (y+2)%height]) >>2
-
-# Mettre à jour les valeurs dans 'a' avec les valeurs calculées
-    buffer[0:width, 0:height] = result[0:width, 0:height]     
+from numba import njit, prange
 
 
-for i in range(0,64):
-        colour_map[i][0] = i * 4
-        colour_map[i][1] = 0
-        colour_map[i][2] = 0
-        
-        colour_map[i+128][0] = 255
-        colour_map[i+64][1] = i * 2
-        colour_map[i+64][2] = 0
-            
-        colour_map[i+128][0] = 255     
-        colour_map[i+128][1] = 128 + i * 2
-        colour_map[i+128][2] = i * 2
-     
-        colour_map[i+192][0] = 255        
-        colour_map[i+192][1] = 255
-        colour_map[i+192][2] = 128 + i * 2
+class Fire:
+    def __init__(self, app):
+        self.app = app
 
-# pygame setup
-pygame.init()
-screen = pygame.display.set_mode((screen_width, screen_height),0,8)
+        self.width = 800
+        self.height = 600
+        self.screen_width = 800
+        self.screen_height = 600
+        self.colour_map = np.zeros((256, 3), np.uint8)
 
-surface = pygame.Surface((width,height),0,8)
-surface.set_palette(colour_map)
+        self.set_palette()  # Set the colour palette
+        self.surface = pygame.Surface((self.width, self.height), 0, 8)
 
-buffer = pygame.surfarray.array2d(surface)
-clock = pygame.time.Clock()
+        self._buffer = pygame.surfarray.array2d(self.surface)
 
-running = True
+        self.surface.set_palette(self.colour_map)
 
-while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    def set_palette(self):
+        for i in range(0, 64):
+            self.colour_map[i][0] = i * 2
+            self.colour_map[i][1] = 0
+            self.colour_map[i][2] = 0
 
-    clock.tick()
-    print(clock.get_fps())
+            self.colour_map[i + 64][0] = 128 + i * 2
+            self.colour_map[i + 64][1] = i * 2
+            self.colour_map[i + 64][2] = 0
 
-    add_fire()
-    fire()
-    pygame.surfarray.blit_array(surface, buffer)
-    temp = pygame.transform.scale(surface, screen.get_size())
-    screen.blit(temp,(0,0))
+            self.colour_map[i + 128][0] = 255
+            self.colour_map[i + 128][1] = 128 + i * 2
+            self.colour_map[i + 128][2] = i * 2
 
-    # flip() the display to put your work on screen
-    pygame.display.flip()
+            self.colour_map[i + 192][0] = 255
+            self.colour_map[i + 192][1] = 255
+            self.colour_map[i + 192][2] = 128 + i * 2
 
-pygame.quit()
+    @staticmethod
+    @njit(parallel=True, fastmath=True)
+    def add_fire(buffer):
 
+        for x in prange(0, buffer.shape[0]):
+            buffer[x][buffer.shape[1] - 1] = random.randint(0, 255)
 
+        return buffer
 
-     
+    @staticmethod
+    @njit(parallel=True, fastmath=True)
+    def fire(width, height, buffer):
+        # Créer un tableau pour stocker les valeurs calculées
+        result = np.zeros_like(buffer)
+
+        # Parcourir les indices x et y, mais exclure les bords
+        for x in prange(0, width):
+            for y in prange(0, height):
+                result[x, y] = (
+                    buffer[(x - 1) % width, (y + 1) % height]
+                    + buffer[x, (y + 1) % height]
+                    + buffer[(x + 1) % width, (y + 1) % height]
+                    + buffer[x, (y + 2) % height]
+                ) >> 2
+
+        return result
+
+    def update(self):
+        self._buffer = self.add_fire(self._buffer)
+        self._buffer = self.fire(self.width, self.height, self._buffer)
+
+    def draw(self):
+
+        pygame.surfarray.blit_array(self.surface, self._buffer)
+        self.app.screen.blit(self.surface, (0, 0))
